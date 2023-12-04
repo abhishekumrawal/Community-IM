91e149e1e29cc8696546720dbad9e1d8f736430e
Eliot Robson <eliot.robson24@gmail.com>
2023-12-03 21:05:41 -0600

    Indentation

--- im_functions/propagation/independent_cascade.py
+++ im_functions/propagation/independent_cascade.py
@@ -10,13 +10,15 @@ Implement independent cascade model
 __author__ = """Hung-Hsuan Chen (hhchen@psu.edu)"""
 
 import copy
-import networkx as nx
 import random
 
-__all__ = ['independent_cascade']
+import networkx as nx
 
-def independent_cascade(G, seeds, *, steps=0, random_seed = None):
-  """Return the active nodes of each diffusion step by the independent cascade
+__all__ = ["independent_cascade"]
+
+
+def independent_cascade(G, seeds, *, steps=0, random_seed=None):
+    """Return the active nodes of each diffusion step by the independent cascade
   model
 
   Parameters
@@ -54,81 +56,91 @@ def independent_cascade(G, seeds, *, steps=0, random_seed = None):
       Influential nodes in a diffusion model for social networks.
       In Automata, Languages and Programming, 2005.
   """
-  if type(G) == nx.MultiGraph or type(G) == nx.MultiDiGraph:
-      raise Exception( \
-          "independent_cascade() is not defined for graphs with multiedges.")
+    if type(G) == nx.MultiGraph or type(G) == nx.MultiDiGraph:
+        raise Exception(
+            "independent_cascade() is not defined for graphs with multiedges."
+        )
 
-  rand_gen = random.Random(random_seed)
+    rand_gen = random.Random(random_seed)
 
-  # make sure the seeds are in the graph
-  for s in seeds:
-    if s not in G.nodes():
-      raise Exception("seed", s, "is not in graph")
+    # make sure the seeds are in the graph
+    for s in seeds:
+        if s not in G.nodes():
+            raise Exception("seed", s, "is not in graph")
 
-  # change to directed graph
-  if not G.is_directed():
-    DG = G.to_directed()
-  else:
-    DG = copy.deepcopy(G)
+    # change to directed graph
+    if not G.is_directed():
+        DG = G.to_directed()
+    else:
+        DG = copy.deepcopy(G)
 
-  # init activation probabilities
-  for e in DG.edges():
-    if 'act_prob' not in DG[e[0]][e[1]]:
-      DG[e[0]][e[1]]['act_prob'] = 0.1
-    elif DG[e[0]][e[1]]['act_prob'] > 1:
-      raise Exception("edge activation probability:", \
-          DG[e[0]][e[1]]['act_prob'], "cannot be larger than 1")
+    # init activation probabilities
+    for e in DG.edges():
+        if "act_prob" not in DG[e[0]][e[1]]:
+            DG[e[0]][e[1]]["act_prob"] = 0.1
+        elif DG[e[0]][e[1]]["act_prob"] > 1:
+            raise Exception(
+                "edge activation probability:",
+                DG[e[0]][e[1]]["act_prob"],
+                "cannot be larger than 1",
+            )
 
-  # perform diffusion
-  A = copy.deepcopy(seeds)  # prevent side effect
-  if steps <= 0:
-    # perform diffusion until no more nodes can be activated
-    return _diffuse_all(DG, A, rand_gen)
-  # perform diffusion for at most "steps" rounds
-  return _diffuse_k_rounds(DG, A, steps, rand_gen)
+    # perform diffusion
+    A = copy.deepcopy(seeds)  # prevent side effect
+    if steps <= 0:
+        # perform diffusion until no more nodes can be activated
+        return _diffuse_all(DG, A, rand_gen)
+    # perform diffusion for at most "steps" rounds
+    return _diffuse_k_rounds(DG, A, steps, rand_gen)
+
 
 def _diffuse_all(G, A, rand_gen):
-  tried_edges = set()
-  layer_i_nodes = [ ]
-  layer_i_nodes.append([i for i in A])  # prevent side effect
-  while True:
-    len_old = len(A)
-    (A, activated_nodes_of_this_round, cur_tried_edges) = \
-        _diffuse_one_round(G, A, tried_edges, rand_gen)
-    layer_i_nodes.append(activated_nodes_of_this_round)
-    tried_edges = tried_edges.union(cur_tried_edges)
-    if len(A) == len_old:
-      break
-  return layer_i_nodes
+    tried_edges = set()
+    layer_i_nodes = []
+    layer_i_nodes.append([i for i in A])  # prevent side effect
+    while True:
+        len_old = len(A)
+        (A, activated_nodes_of_this_round, cur_tried_edges) = _diffuse_one_round(
+            G, A, tried_edges, rand_gen
+        )
+        layer_i_nodes.append(activated_nodes_of_this_round)
+        tried_edges = tried_edges.union(cur_tried_edges)
+        if len(A) == len_old:
+            break
+    return layer_i_nodes
+
 
 def _diffuse_k_rounds(G, A, steps, rand_gen):
-  tried_edges = set()
-  layer_i_nodes = [ ]
-  layer_i_nodes.append([i for i in A])
-  while steps > 0 and len(A) < len(G):
-    len_old = len(A)
-    (A, activated_nodes_of_this_round, cur_tried_edges) = \
-        _diffuse_one_round(G, A, tried_edges, rand_gen)
-    layer_i_nodes.append(activated_nodes_of_this_round)
-    tried_edges = tried_edges.union(cur_tried_edges)
-    if len(A) == len_old:
-      break
-    steps -= 1
-  return layer_i_nodes
+    tried_edges = set()
+    layer_i_nodes = []
+    layer_i_nodes.append([i for i in A])
+    while steps > 0 and len(A) < len(G):
+        len_old = len(A)
+        (A, activated_nodes_of_this_round, cur_tried_edges) = _diffuse_one_round(
+            G, A, tried_edges, rand_gen
+        )
+        layer_i_nodes.append(activated_nodes_of_this_round)
+        tried_edges = tried_edges.union(cur_tried_edges)
+        if len(A) == len_old:
+            break
+        steps -= 1
+    return layer_i_nodes
+
 
 def _diffuse_one_round(G, A, tried_edges, rand_gen):
-  activated_nodes_of_this_round = set()
-  cur_tried_edges = set()
-  for s in A:
-    for nb in G.successors(s):
-      if nb in A or (s, nb) in tried_edges or (s, nb) in cur_tried_edges:
-        continue
-      if _prop_success(G, s, nb, rand_gen):
-        activated_nodes_of_this_round.add(nb)
-      cur_tried_edges.add((s, nb))
-  activated_nodes_of_this_round = list(activated_nodes_of_this_round)
-  A.extend(activated_nodes_of_this_round)
-  return A, activated_nodes_of_this_round, cur_tried_edges
+    activated_nodes_of_this_round = set()
+    cur_tried_edges = set()
+    for s in A:
+        for nb in G.successors(s):
+            if nb in A or (s, nb) in tried_edges or (s, nb) in cur_tried_edges:
+                continue
+            if _prop_success(G, s, nb, rand_gen):
+                activated_nodes_of_this_round.add(nb)
+            cur_tried_edges.add((s, nb))
+    activated_nodes_of_this_round = list(activated_nodes_of_this_round)
+    A.extend(activated_nodes_of_this_round)
+    return A, activated_nodes_of_this_round, cur_tried_edges
+
 
 def _prop_success(G, src, dest, rand_gen):
-  return rand_gen.random() <= G[src][dest]['act_prob']
+    return rand_gen.random() <= G[src][dest]["act_prob"]
