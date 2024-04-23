98826f99940b4c52ac9e9f53629831f0125da108
Eliot Robson <eliot.robson24@gmail.com>
2024-04-23 15:20:35 -0500

    Add back more comments

--- im_functions/ccelfpp1_im.py
+++ im_functions/ccelfpp1_im.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 18:29:52 2018
+
+@author: abhishek.umrawal
+"""
+
+# importing required built-in modules
 import json
 import logging
 import os
@@ -8,6 +17,7 @@ import timeit
 import numpy as np
 from networkx.algorithms.community import is_partition, modularity, partition_quality
 
+# importing required user-defined modules
 from im_functions.celfpp_im import celfpp_im
 from im_functions.community_detection import community_detection
 from im_functions.progressive_budgeting import progressive_budgeting
@@ -24,40 +34,47 @@ def ccelfpp1_im(
     community_size_threshold=0.01,
     all_upto_budget=True,
 ):
+    # start the timer
     start = timeit.default_timer()
 
+    # results folder
     results_folder = "results/results_" + diffusion_model + "_" + weighting_scheme
 
+    # initialize the run_time info dictionary
     runtime_info = {}
 
+    # set a random seed
     np.random.seed(int(random.uniform(0, 1000000)))
 
+    # set budget as number of nodes if the user gives a budget > number of nodes
     budget = min(budget, len(network.nodes))
 
+    # initialize empty output lists
     final_best_seed_sets = []
     final_exp_influences = []
 
+    # creating pickle files folder within the results folder
     results_folder_pickle_files = (
         results_folder + os.sep + "results" + network.name + os.sep + "pickle_files"
     )
-
     if not os.path.exists(results_folder_pickle_files):
         os.makedirs(results_folder_pickle_files)
 
+    # creating log files folder within the results folder
     results_folder_log_files = (
         results_folder + os.sep + "results" + network.name + os.sep + "log_files"
     )
-
     if not os.path.exists(results_folder_log_files):
         os.makedirs(results_folder_log_files)
 
+    # creating runtime files folder within the results folder
     results_folder_runtime_files = (
         results_folder + os.sep + "results" + network.name + os.sep + "runtime_files"
     )
-
     if not os.path.exists(results_folder_runtime_files):
         os.makedirs(results_folder_runtime_files)
 
+    # part 1: community detection
     if communities == []:
         logging.info(
             "Part 1: Detecting communities using " + community_method + " method."
@@ -75,6 +92,7 @@ def ccelfpp1_im(
         runtime_info["community_detection"] = 0
         logging.info("Time taken in community detection is " + str(0) + " seconds.")
 
+    # community statistics
     num_communities = len(communities)
     sizes_communities = [len(community) for community in communities]
     is_valid_partition = is_partition(network, communities)
@@ -92,6 +110,7 @@ def ccelfpp1_im(
     # logging.info('Performance of the partition: ' + str(performance_score)+'.')
     logging.info("Modularity of the partition is " + str(modularity_score) + ".")
 
+    # finding small communities and merging them together
     small_communities = [
         community
         for community in communities
@@ -134,15 +153,19 @@ def ccelfpp1_im(
         + "."
     )
 
+    # using new communities as the final communities for the next step
     communities = new_communities
 
+    # part 2: celfpp within each community
     logging.info("Part 2: Solving influence maximization for each community.")
     all_communities_results = {}
     runtime_info["celfpp"] = 0
     for i, community in enumerate(communities):
         starti = timeit.default_timer()
         logging.info("Community-CELF++ is working on community " + str(i) + ".")
-        sub_network = network.subgraph(community).copy()
+        sub_network = (
+            network.subgraph(community).copy()
+        )  # .copy() makes a subgraph with its own copy of the edge/node attributes
         sub_network.name = network.name + "_community_" + str(i)
         budget_for_greedy = min(len(sub_network.nodes), budget)
         best_seed_set, exp_influence, runtime = celfpp_im(
@@ -173,6 +196,7 @@ def ccelfpp1_im(
 
     # print(all_communities_results)
 
+    # part 3.1: progressive budgeting
     logging.info("Part 3.1: Solving the progressive budgeting problem.")
     startk = timeit.default_timer()
     final_best_seed_sets = progressive_budgeting(all_communities_results, budget)
@@ -193,6 +217,7 @@ def ccelfpp1_im(
         + " seconds."
     )
 
+    # saving runtime info
     runtime_info["ccelfpp1"] = (
         runtime_info["community_detection"]
         + runtime_info["celfpp"]
@@ -202,6 +227,7 @@ def ccelfpp1_im(
     with open(fstr, "w") as f:
         f.write(json.dumps(runtime_info))
 
+    # saving results
     if all_upto_budget:
         results = {
             "budget": budget,
